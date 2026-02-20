c2425bec5e53978eb3acc2a7289eca62ebe06d58
Shreyash Kolhe <shreyashkolhe56@gmail.com>
2026-02-20 16:49:55 +0530

    initialize the project

---
+++ liftNshift_Backend/src/main/java/com/example/liftNshift_Backend/LiftNshiftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.liftNshift_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftNshiftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftNshiftBackendApplication.class, args);
+	}
+
+}
---
+++ liftNshift_Backend/src/test/java/com/example/liftNshift_Backend/LiftNshiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liftNshift_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftNshiftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
