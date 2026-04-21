18ef1aaf3e1f0de40d7838125a08ced4e9cb8b5f
Shreyash Kolhe <shreyashkolhe56@gmail.com>
2026-04-22 01:22:09 +0530

    Add jpa query to repo to make abel to admin perform operation on db

--- shifting/src/main/java/com/shifting/repository/PredefinedItemRepository.java
+++ shifting/src/main/java/com/shifting/repository/PredefinedItemRepository.java
@@ -3,8 +3,10 @@ package com.shifting.repository;
 import com.shifting.model.PredefinedItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PredefinedItemRepository
         extends JpaRepository<PredefinedItem, Long> {
+    Optional<PredefinedItem> findByName(String name);
 }
